6735a1a86e25a4007ea42cdf2d909af8222cf1e6
katiaITS <katia.strusi@consorziocopernico.it>
2025-10-10 21:20:51 +0200

    Ordinamento modello catalogo alfabetico

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User #serve per sapere chi ha caricato cosa
+from django.utils.text import slugify
 
 class Catalogo(models.Model):
     nome=models.JSONField(
@@ -11,7 +12,9 @@ class Catalogo(models.Model):
     slug=models.SlugField(
         unique=True,
         max_length=100,
-        verbose_name='Slug URL'
+        blank=True,
+        verbose_name='Slug URL',
+        help_text='Lascia vuoto per generazione automatica dal nome italiano'
     )
 
     #immagine rappresentativa del catalogo
@@ -23,11 +26,11 @@ class Catalogo(models.Model):
     )
 
     #Campo per decidere l'ordine di visualizzazione - l'ordine lo decidiamo noi in base al numero - valutare autoincremente con riordinamento a posteriori)
-    order = models.IntegerField(
-        default=0,
-        verbose_name='Ordine',
-        help_text='Numero per ordinamento (dal più basso al più alto)'
-    )
+    #order = models.IntegerField(
+       # default=0,
+     #   verbose_name='Ordine',
+     #   help_text='Numero per ordinamento (dal più basso al più alto)'
+    #)
 
     #nascondere catalogo quando non più attivo (per storico o eventuale riattivazione)
     is_active = models.BooleanField(
@@ -46,11 +49,11 @@ class Catalogo(models.Model):
         verbose_name='Ultima Modifica'
     )
 
-    #Per i nomi nell'admin e per l'ordinamento
+    #Per i nomi nell'admin e per l'ordinamento alfabetico
     class Meta:
         verbose_name = 'Catalogo'
         verbose_name_plural = 'Cataloghi'
-        ordering = ['order', 'nome']
+        ordering = ['slug']
 
     #Restituisce il nome italiano del catalogo
     def __str__(self):
@@ -59,3 +62,19 @@ class Catalogo(models.Model):
     #Metodo che in base alla lingua restituisce il nome corretto, se non specificato di default da italiano
     def get_nome(self, lingua='it'):
         return self.nome.get(lingua, self.nome.get('it', 'N/D')) #cerca la lingua richiesta se non esiste cerca it sennò N/D
+
+    #Ovverride del metodo save per creare in automatico lo slug in italiano
+    def save(self, *args, **kwargs):
+        if not self.slug: #se slug è vuoto
+            #prende il nome in italiano e genera slug
+            nome_italiano=self.nome.get('it','')
+            self.slug= slugify(nome_italiano)
+
+            #Gestione duplicati - inserisce numero
+            original_slug = self.slug
+            counter = 1
+            while Catalogo.objects.filter(slug=self.slug).exclude(pk=self.pk).exists():
+                self.slug = f"{original_slug}-{counter}"
+                counter += 1
+        
+        super().save(*args, **kwargs)
